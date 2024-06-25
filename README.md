2f9c5468b209ac4cddb8cbeaf926515c32e19f87
Atuma David <atumadavid35@gmail.com>
2024-06-25 12:59:15 +0100

    auth,convex mutation, db

--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 - **Backend**: Convex (https://docs.convex.dev/quickstart/nextjs)
   - _Install_: npm install convex
   - _Intialize Convex Project_: npx convex dev
-- **Authentication**: Clerk
+- **Authentication**: Clerk (https://clerk-docs-1swzi0cqq.clerkpreview.com/quickstarts/nextjs/app-router)
   - - _Install_: npm install @clerk/nextjs
+- **UI Library**: shadcn (https://ui.shadcn.com/docs)
---
+++ convex/files.ts
@@ -0,0 +1,15 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+// mutation: an endpoint you can call from your FE react code
+// it will do some kind of modification such as store an entry into convex database
+export const creatFile = mutation({
+  args: {
+    name: v.string(),
+  },
+  async handler(ctx, args) {
+    await ctx.db.insert("files", {
+      name: args.name,
+    });
+  },
+});
